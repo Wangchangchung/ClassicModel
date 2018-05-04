9d2500500b2ce42c3bd25b2a2ca94bc69b0763aa
Wangchangchung <1132629383@qq.com>
2018-05-04 17:02:03 +0800

    特征降维

--- non-supervision/dimensionality-reduction/PCAreduction.py
+++ non-supervision/dimensionality-reduction/PCAreduction.py
@@ -4,14 +4,12 @@
 是最为经典个实用的特征降维技术,特别时在辅助图像识别方面有突出的表现
 '''
 
-
 import pandas
 import numpy
 from sklearn.svm import LinearSVC
 from sklearn.decomposition import PCA
 from matplotlib import pyplot as plt
 from sklearn.metrics import classification_report
-
 
 digits_train = pandas.read_csv("http://archive.ics.uci.edu/ml/machine-learning-databases/optdigits/optdigits.tra", header=None)
 
@@ -27,7 +25,7 @@ X_pca = estimator.fit_transform(X_digits)
 
 def plot_pca_scatter():
     colors = ['black', 'blue', 'purple', 'yellow', 'white', 'red', 'lime', 'cyan', 'orange', 'gray']
-    for i in range(len(colors)):
+    for i in range(len(colors)):   # python  3 中的用法时时直接使用range遍历, 但是在2 中使用的是xrange
         px = X_pca[:, 0][y_digits.as_matrix() == i]
         py = X_pca[:, 1][y_digits.as_matrix() == i]
         plt.scatter(px, py, c=colors[i])
@@ -76,6 +74,14 @@ print(pca_svc.score(pca_X_test, y_test))
 print(classification_report(y_test, pca_y_predict, target_names=numpy.arange(10).astype(str)))
 
 
+'''
+特点分析:
+
+降维/压缩问题则是选取数据具有代表性的特征,在保持数据多样性(Variance) 的基础上,规避掉大量的
+特征冗余和噪声,不过这个过程也很有可能会损失一些有用的模式信息,经过大量的实践证明
+相较于损失的少部分模型性能,维度压缩能够节省大量用于模型训练的时间,这样一来,使得PCA所带来的模型综合效率变得更加划算
+
+'''
 
 
 
